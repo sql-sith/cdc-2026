dd395232816d56e8bc11e42dd4c9dbc6eea33b61
Chris Leonard <chris@databaseguy.com>
2026-07-28 20:08:44 -0500

    game utils

---
+++ topics-summer/07-unit-tests/00-game-utils/test_game_utils.py
@@ -0,0 +1,6 @@
+# File name: test_game_utils.py
+
+from game_utils import calculate_xp, is_player_alive
+
+# test xp for various levels and monsters killed
+# test player life with various health levels
