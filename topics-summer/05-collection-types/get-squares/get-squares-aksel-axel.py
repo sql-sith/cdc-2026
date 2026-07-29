7527e4924891d47c9a6af6adf0b0c08e4a4721f6
Aksel-Rasmussen <146550087+Aksel-Rasmussen@users.noreply.github.com>
2026-07-29 11:40:35 -0500

    Fix NameError arising from incorrect rename; Did the thing Copilot wanted

--- topics-summer/05-collection-types/get-squares/get-squares-aksel-axel.py
+++ topics-summer/05-collection-types/get-squares/get-squares-aksel-axel.py
@@ -3,34 +3,43 @@
 def get_squares(numbers):
     """Get the square of all the elements of an iterable.
     """
+    # >> Setup and dictionary checking <<
     new_numbers = []
     keys_of_dict = []
+    numbers_was_dict = False
     if isinstance(numbers, dict):
+        numbers_was_dict = True
+        # ^ Because `numbers` becomes an itterable in this
+        #   if statement, it's not possible to rely on the
+        #   `isinstance` used lower down in this function.
         keys_of_dict = list(numbers)
         numbers = numbers.values()
+    # >> Squaring <<
     for number in numbers:
         try:
             new_numbers.append(number ** 2)
         except TypeError:
             new_numbers.append(None)
-    if isinstance(numbers, set):
-        return set(new_numbers)
-    if isinstance(numbers, list):
-        return new_numbers
-    if isinstance(numbers, tuple):
-        return tuple(new_numbers)
-    if len(keys) > 0:
+    # >> Return the same type inputted <<
+    if numbers_was_dict:
         new_dict = {}
         for k, v in zip(
             keys_of_dict,
             new_numbers):
             new_dict[k] = v
         return new_dict
+    if isinstance(numbers, set):
+        return set(new_numbers)
+    if isinstance(numbers, list):
+        return new_numbers
+    if isinstance(numbers, tuple):
+        return tuple(new_numbers)
     if isinstance(numbers, str):
         return "I hate being squared."
     if isinstance(numbers, bytes):
         return b"I hate being squared."
-    return new_numbers # Fallback
+    # >> Fallback to returning a list of squared values <<
+    return new_numbers
 
 
 if __name__ == "__main__":
